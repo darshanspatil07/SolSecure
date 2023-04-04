515022fdecd13c1f3820afe95383813cbfb5e7cf
Darshan Shanthveer Patil <90526766+darshanspatil07@users.noreply.github.com>
2023-04-04 03:06:56 +0000

    Python file to get sol version from solidity files

---
+++ getSolVersion.py
@@ -0,0 +1,16 @@
+import sys
+
+def get_sol_version(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            if line.startswith("pragma solidity "):
+                version = line.split(" ")[2].replace(";"," ")
+                return version
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please provide the Solidity file path as an argument.")
+    else:
+        file_path = sys.argv[1]
+        sol_version = get_sol_version(file_path)
+        print(f"The Solidity version used in {file_path} is {sol_version}")
