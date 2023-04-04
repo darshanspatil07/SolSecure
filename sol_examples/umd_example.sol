e28c81b7068b925bc0c0bb8b71c0d96f02914071
Darshan Shanthveer Patil <90526766+darshanspatil07@users.noreply.github.com>
2023-04-03 21:57:13 -0500

    Update umd_example.sol

--- sol_examples/umd_example.sol
+++ sol_examples/umd_example.sol
@@ -2,7 +2,7 @@
 // by Michael Hicks, University of Maryland.
 // https://www.cs.umd.edu/~mwh/se-tutorial/symbolic-exec.pdf
 contract SymExExample {
-
+pragma solidity 0.5.0;
     function test_me(int a, int b, int c) public pure {
         int x = 0;
         int y = 0;
